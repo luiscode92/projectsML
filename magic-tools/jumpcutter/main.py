3b9736a867a3f3607162fe6ef98e557b7c0c2ff2
luis <luis@Daniel-MacBook-Air.local>
2023-06-21 16:11:07 -0500

    adding several video formats

--- magic-tools/jumpcutter/main.py
+++ magic-tools/jumpcutter/main.py
@@ -1,29 +1,26 @@
 import streamlit as st
 import base64
 import moviepy.editor as mpy
-from jump_cutter import jump_cutter # let's assume that we've moved your code into a function inside this module
+from jump_cutter import jump_cutter
 
 st.title("Cortar silencios - Demo")
-uploaded_file = st.file_uploader("Cargar video en .mp4", type=['mp4', 'avi'])
+uploaded_file = st.file_uploader("Cargar video en")
 
-# Parameters for the jump_cutter function
-#silent_threshold = st.slider("Silent threshold", min_value=0.0, max_value=1.0, value=0.03, step=0.01)
-#sounded_speed = st.slider("Sounded speed", min_value=0.0, max_value=5.0, value=1.0, step=0.1)
-#silent_speed = st.slider("Silent speed", min_value=0.0, max_value=999999.0, value=5.0, step=1.0)
-#frame_margin = st.slider("Frame margin", min_value=0, max_value=10, value=1)
-#frame_rate = st.slider("Frame rate", min_value=0, max_value=60, value=30)
+if uploaded_file is not None:
+    # Get the file extension
+    file_extension = uploaded_file.name.split(".")[-1]
 
 if st.button('Cortar video'):
-    # save the uploaded file to the local filesystem
-    with open("input.mp4", "wb") as f:
+    # save the uploaded file to the local filesystem with the correct extension
+    input_file = f"input.{file_extension}"
+    with open(input_file, "wb") as f:
         f.write(uploaded_file.getbuffer())
 
-    # call the function
+    # call the function with the correct extension for the output
     jump_cutter(
-        input_file="input.mp4",
-        output_file="output.mp4",
+        input_file=input_file,
+        output_file=f"output.{file_extension}",
     )
-
 
 # Add a lot of space before the footer
 for _ in range(25):
